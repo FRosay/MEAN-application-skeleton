c770aae9938f2cffb1b9d48035994a5822b1faad
Julien Leveau <j.leveau@cis-valley.fr>
2018-01-31 15:51:07 +0100

    create a basic Node/Angular2 server

--- back/app.js
+++ back/app.js
@@ -9,37 +9,25 @@ const logger = require('morgan');
 const app = express();
 
 const api = require('./routes/api');
-
+app.use(express.static(path.resolve(__dirname + '/../front/dist')));
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '../front/badmintonApp/src/index.html');
+    res.sendFile(path.resolve(__dirname + '/../front/dist/index.html'));
 });
 
 app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
 });
 
 module.exports = app;
