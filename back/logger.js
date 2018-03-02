07b4d775a3620d2bb60fedf252e3fe63b70e30b3
jleveau <julien.leveau@etu.u-bordeaux.fr>
2018-03-02 17:47:05 +0100

    creating a debug logger and removing the error logger for the test environement

--- back/logger.js
+++ back/logger.js
@@ -3,15 +3,17 @@ const winston = require('winston');
 let transports = [];
 if (process.env.NODE_ENV === 'test') {
   transports = [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })];
+    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: 'debug.log', level: 'debug' })];
 }
 if (process.env.NODE_ENV === 'dev') {
   transports = [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
-    new winston.transports.File({ filename: 'error.log', level: 'error' })
-  ]
-
+    new winston.transports.File({ filename: 'debug.log', level: 'debug' })];
+}
+if (process.env.NODE_ENV === 'prod') {
+  transports = [new winston.transports.File({ filename: 'error.log', level: 'error' })];
 }
 
 const logger = winston.createLogger({
